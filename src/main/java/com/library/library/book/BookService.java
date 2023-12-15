e6af305eac77331dbafde2a5941c328def37016d
Patryk Badowski <patryk_badowski@o2.pl>
2023-12-15 22:15:39 +0100

    Fix code errors

--- src/main/java/com/library/library/book/BookService.java
+++ src/main/java/com/library/library/book/BookService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,11 +17,11 @@ class BookService {
 
     final private DateTimeFormatter dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
 
-    private final List<BookEntity> books = Arrays.asList(
+    private final List<BookEntity> books = new ArrayList<>(Arrays.asList(
             new BookEntity(1L, "Ogniem i mieczem", "Henryk Sienkiewicz", LocalDate.of(1884, 12, 23)),
             new BookEntity(2L, "Potop", "Henryk Sienkiewicz", LocalDate.of(1886, 3, 17)),
             new BookEntity(3L, "Harry Potter", "J.K. Rowling", LocalDate.of(1997, 6, 26))
-    );
+    ));
 
     List<Book> getBooks() {
         return books.stream()
